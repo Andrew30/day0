7f9dad44ec199a663352b49532ad9d13bcab21a1
Andrew Young <andrewyang56@gmail.com>
2017-01-17 12:25:24 +0300

    Initialize Subtract numbers

--- calculator.py
+++ calculator.py
@@ -5,12 +5,20 @@ class Calculator(object):
     def add(self, x, y):
         return x + y
 
+    def Subtract(self, x, y):
+        return x - y
+
 
 class tddexample(unittest.TestCase):
     def test_calculator_add_method_returns_correct_result(self):
         calc = Calculator()
         result = calc.add(2, 2)
         self.assertEqual(4, result)
+
+    def test_calculator_Subtract_method_returns_correct_result(self):
+        calc = Calculator()
+        result = calc.Subtract(5, 2)
+        self.assertEqual(3, result)
 
 
 if __name__ == '__main__':
